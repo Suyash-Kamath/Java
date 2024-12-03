2ef651c7d6dea958cebec05f801ef59cfb5dfdbc
Suyash-Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2024-12-03 21:22:39 +0530

    Update Main.java

--- OOPs/Introduction-to-Classes/src/com/suyash/introduction/Main.java
+++ OOPs/Introduction-to-Classes/src/com/suyash/introduction/Main.java
@@ -186,6 +186,12 @@ public class Main {
 
         Student random2 = new Student()  ;
         System.out.println(random2.name);
+
+        Student one = new Student();
+        Student two = one;
+
+        one.name = "one something";
+        System.out.println(two.name);
     }
 }
 
